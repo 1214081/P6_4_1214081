74946342cd17f978d4fac368bcded3d2e6a187ea
Ibrohim Mubarok <114393418+1214081@users.noreply.github.com>
2022-10-29 04:23:40 +0700

    Add files via upload

---
+++ P6_4_1214081/P6_4_1214081/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿namespace P6_4_1214081
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbNama = new System.Windows.Forms.Label();
+            this.lbPendidikanTerakhir = new System.Windows.Forms.Label();
+            this.lbKotaAsal = new System.Windows.Forms.Label();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.lbBeratSekarang = new System.Windows.Forms.Label();
+            this.lbBeratTahunKemaren = new System.Windows.Forms.Label();
+            this.tbNama = new System.Windows.Forms.TextBox();
+            this.tbPendidikanTerakhir = new System.Windows.Forms.TextBox();
+            this.tbKotaAsal = new System.Windows.Forms.TextBox();
+            this.tbEmail = new System.Windows.Forms.TextBox();
+            this.tbBeratSekarang = new System.Windows.Forms.TextBox();
+            this.tbBeratTahunKemaren = new System.Windows.Forms.TextBox();
+            this.epCorrect = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWrong = new System.Windows.Forms.ErrorProvider(this.components);
+            this.epWarning = new System.Windows.Forms.ErrorProvider(this.components);
+            this.btTampilkan = new System.Windows.Forms.Button();
+            this.tbUsernameValorant = new System.Windows.Forms.TextBox();
+            this.lbUsernameValorant = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbNama
+            // 
+            this.lbNama.AutoSize = true;
+            this.lbNama.Location = new System.Drawing.Point(133, 53);
+            this.lbNama.Name = "lbNama";
+            this.lbNama.Size = new System.Drawing.Size(35, 13);
+            this.lbNama.TabIndex = 0;
+            this.lbNama.Text = "Nama";
+            // 
+            // lbPendidikanTerakhir
+            // 
+            this.lbPendidikanTerakhir.AutoSize = true;
+            this.lbPendidikanTerakhir.Location = new System.Drawing.Point(133, 123);
+            this.lbPendidikanTerakhir.Name = "lbPendidikanTerakhir";
+            this.lbPendidikanTerakhir.Size = new System.Drawing.Size(102, 13);
+            this.lbPendidikanTerakhir.TabIndex = 1;
+            this.lbPendidikanTerakhir.Text = "Pendidikan Terakhir";
+            // 
+            // lbKotaAsal
+            // 
+            this.lbKotaAsal.AutoSize = true;
+            this.lbKotaAsal.Location = new System.Drawing.Point(133, 158);
+            this.lbKotaAsal.Name = "lbKotaAsal";
+            this.lbKotaAsal.Size = new System.Drawing.Size(52, 13);
+            this.lbKotaAsal.TabIndex = 2;
+            this.lbKotaAsal.Text = "Kota Asal";
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Location = new System.Drawing.Point(133, 193);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(32, 13);
+            this.lbEmail.TabIndex = 4;
+            this.lbEmail.Text = "Email";
+            // 
+            // lbBeratSekarang
+            // 
+            this.lbBeratSekarang.AutoSize = true;
+            this.lbBeratSekarang.Location = new System.Drawing.Point(133, 228);
+            this.lbBeratSekarang.Name = "lbBeratSekarang";
+            this.lbBeratSekarang.Size = new System.Drawing.Size(139, 13);
+            this.lbBeratSekarang.TabIndex = 6;
+            this.lbBeratSekarang.Text = "Berat Badan Sekarang (KG)";
+            // 
+            // lbBeratTahunKemaren
+            // 
+            this.lbBeratTahunKemaren.AutoSize = true;
+            this.lbBeratTahunKemaren.Location = new System.Drawing.Point(133, 263);
+            this.lbBeratTahunKemaren.Name = "lbBeratTahunKemaren";
+            this.lbBeratTahunKemaren.Size = new System.Drawing.Size(169, 13);
+            this.lbBeratTahunKemaren.TabIndex = 7;
+            this.lbBeratTahunKemaren.Text = "Berat Badan Tahun Kemaren (KG)";
+            // 
+            // tbNama
+            // 
+            this.tbNama.Location = new System.Drawing.Point(343, 50);
+            this.tbNama.Name = "tbNama";
+            this.tbNama.Size = new System.Drawing.Size(216, 20);
+            this.tbNama.TabIndex = 8;
+            this.tbNama.Leave += new System.EventHandler(this.tbNama_Leave);
+            // 
+            // tbPendidikanTerakhir
+            // 
+            this.tbPendidikanTerakhir.Location = new System.Drawing.Point(343, 120);
+            this.tbPendidikanTerakhir.Name = "tbPendidikanTerakhir";
+            this.tbPendidikanTerakhir.Size = new System.Drawing.Size(216, 20);
+            this.tbPendidikanTerakhir.TabIndex = 9;
+            this.tbPendidikanTerakhir.Leave += new System.EventHandler(this.tbPendidikanTerakhir_Leave);
+            // 
+            // tbKotaAsal
+            // 
+            this.tbKotaAsal.Location = new System.Drawing.Point(343, 155);
+            this.tbKotaAsal.Name = "tbKotaAsal";
+            this.tbKotaAsal.Size = new System.Drawing.Size(216, 20);
+            this.tbKotaAsal.TabIndex = 10;
+            this.tbKotaAsal.Leave += new System.EventHandler(this.tbKotaAsal_Leave);
+            // 
+            // tbEmail
+            // 
+            this.tbEmail.Location = new System.Drawing.Point(343, 190);
+            this.tbEmail.Name = "tbEmail";
+            this.tbEmail.Size = new System.Drawing.Size(216, 20);
+            this.tbEmail.TabIndex = 12;
+            this.tbEmail.Leave += new System.EventHandler(this.tbEmail_Leave);
+            // 
+            // tbBeratSekarang
+            // 
+            this.tbBeratSekarang.Location = new System.Drawing.Point(343, 225);
+            this.tbBeratSekarang.Name = "tbBeratSekarang";
+            this.tbBeratSekarang.Size = new System.Drawing.Size(216, 20);
+            this.tbBeratSekarang.TabIndex = 13;
+            this.tbBeratSekarang.Leave += new System.EventHandler(this.tbBeratSekarang_Leave);
+            // 
+            // tbBeratTahunKemaren
+            // 
+            this.tbBeratTahunKemaren.Location = new System.Drawing.Point(343, 260);
+            this.tbBeratTahunKemaren.Name = "tbBeratTahunKemaren";
+            this.tbBeratTahunKemaren.Size = new System.Drawing.Size(216, 20);
+            this.tbBeratTahunKemaren.TabIndex = 14;
+            this.tbBeratTahunKemaren.Leave += new System.EventHandler(this.tbBeratTahunKemaren_Leave);
+            // 
+            // epCorrect
+            // 
+            this.epCorrect.ContainerControl = this;
+            this.epCorrect.Icon = ((System.Drawing.Icon)(resources.GetObject("epCorrect.Icon")));
+            // 
+            // epWrong
+            // 
+            this.epWrong.ContainerControl = this;
+            this.epWrong.Icon = ((System.Drawing.Icon)(resources.GetObject("epWrong.Icon")));
+            // 
+            // epWarning
+            // 
+            this.epWarning.ContainerControl = this;
+            this.epWarning.Icon = ((System.Drawing.Icon)(resources.GetObject("epWarning.Icon")));
+            // 
+            // btTampilkan
+            // 
+            this.btTampilkan.AutoSize = true;
+            this.btTampilkan.Location = new System.Drawing.Point(309, 310);
+            this.btTampilkan.Name = "btTampilkan";
+            this.btTampilkan.Size = new System.Drawing.Size(75, 23);
+            this.btTampilkan.TabIndex = 15;
+            this.btTampilkan.Text = "Tampilkan";
+            this.btTampilkan.UseVisualStyleBackColor = true;
+            this.btTampilkan.Click += new System.EventHandler(this.btTampilkan_Click);
+            // 
+            // tbUsernameValorant
+            // 
+            this.tbUsernameValorant.Location = new System.Drawing.Point(343, 85);
+            this.tbUsernameValorant.Name = "tbUsernameValorant";
+            this.tbUsernameValorant.Size = new System.Drawing.Size(216, 20);
+            this.tbUsernameValorant.TabIndex = 17;
+            this.tbUsernameValorant.Leave += new System.EventHandler(this.tbUsernameValorant_Leave);
+            // 
+            // lbUsernameValorant
+            // 
+            this.lbUsernameValorant.AutoSize = true;
+            this.lbUsernameValorant.Location = new System.Drawing.Point(133, 88);
+            this.lbUsernameValorant.Name = "lbUsernameValorant";
+            this.lbUsernameValorant.Size = new System.Drawing.Size(97, 13);
+            this.lbUsernameValorant.TabIndex = 16;
+            this.lbUsernameValorant.Text = "Username Valorant";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 370);
+            this.Controls.Add(this.tbUsernameValorant);
+            this.Controls.Add(this.lbUsernameValorant);
+            this.Controls.Add(this.btTampilkan);
+            this.Controls.Add(this.tbBeratTahunKemaren);
+            this.Controls.Add(this.tbBeratSekarang);
+            this.Controls.Add(this.tbEmail);
+            this.Controls.Add(this.tbKotaAsal);
+            this.Controls.Add(this.tbPendidikanTerakhir);
+            this.Controls.Add(this.tbNama);
+            this.Controls.Add(this.lbBeratTahunKemaren);
+            this.Controls.Add(this.lbBeratSekarang);
+            this.Controls.Add(this.lbEmail);
+            this.Controls.Add(this.lbKotaAsal);
+            this.Controls.Add(this.lbPendidikanTerakhir);
+            this.Controls.Add(this.lbNama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.epCorrect)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWrong)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epWarning)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNama;
+        private System.Windows.Forms.Label lbPendidikanTerakhir;
+        private System.Windows.Forms.Label lbKotaAsal;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Label lbBeratSekarang;
+        private System.Windows.Forms.Label lbBeratTahunKemaren;
+        private System.Windows.Forms.TextBox tbNama;
+        private System.Windows.Forms.TextBox tbPendidikanTerakhir;
+        private System.Windows.Forms.TextBox tbKotaAsal;
+        private System.Windows.Forms.TextBox tbEmail;
+        private System.Windows.Forms.TextBox tbBeratSekarang;
+        private System.Windows.Forms.TextBox tbBeratTahunKemaren;
+        private System.Windows.Forms.ErrorProvider epCorrect;
+        private System.Windows.Forms.ErrorProvider epWrong;
+        private System.Windows.Forms.ErrorProvider epWarning;
+        private System.Windows.Forms.Button btTampilkan;
+        private System.Windows.Forms.TextBox tbUsernameValorant;
+        private System.Windows.Forms.Label lbUsernameValorant;
+    }
+}
+
---
+++ P6_4_1214081/P6_4_1214081/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P6_4_1214081
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbNama_Leave(object sender, EventArgs e)
+        {
+            if (tbNama.Text == "")
+            {
+                epWarning.SetError(tbNama, "Textbox Nama tidak boleh kosong!");
+                epWrong.SetError(tbNama, "");
+                epCorrect.SetError(tbNama, "");
+            }
+            else
+            {
+
+                if ((tbNama.Text).All(Char.IsLetter))
+                {
+                    epWarning.SetError(tbNama, "");
+                    epWrong.SetError(tbNama, "");
+                    epCorrect.SetError(tbNama, "Betul!");
+                }
+                else
+                {
+                    epWrong.SetError(tbNama, "Inputan Hanya Boleh Huruf!");
+                    epWarning.SetError(tbNama, "");
+                    epCorrect.SetError(tbNama, "");
+                }
+            }
+        }
+
+        private void tbUsernameValorant_Leave(object sender, EventArgs e)
+        {
+            if (tbUsernameValorant.Text == "")
+            {
+                epWarning.SetError(tbUsernameValorant, "Textbox Username Valorant Sekarang tidak boleh kosong!");
+                epWrong.SetError(tbUsernameValorant, "");
+                epCorrect.SetError(tbUsernameValorant, "");
+            }
+            else
+            {
+                if (tbUsernameValorant.Text.Length <= 25)
+                {
+                    epCorrect.SetError(tbUsernameValorant, "Betul!");
+                    epWarning.SetError(tbUsernameValorant, "");
+                    epWrong.SetError(tbUsernameValorant, "");
+                }
+                else
+                {
+                    epCorrect.SetError(tbUsernameValorant, "");
+                    epWarning.SetError(tbUsernameValorant, "");
+                    epWrong.SetError(tbUsernameValorant, "Inputan Hanya Boleh Angka!");
+                }
+            }
+        }
+
+        private void tbPendidikanTerakhir_Leave(object sender, EventArgs e)
+        {
+            if (tbPendidikanTerakhir.Text == "")
+            {
+                epWarning.SetError(tbPendidikanTerakhir, "Textbox Pendidikan Terakhi tidak boleh kosong!");
+                epWrong.SetError(tbPendidikanTerakhir, "");
+                epCorrect.SetError(tbPendidikanTerakhir, "");
+            }
+            else
+            {
+                if ((tbPendidikanTerakhir.Text).All(Char.IsUpper))
+                {
+                    epCorrect.SetError(tbPendidikanTerakhir, "Betul!");
+                    epWarning.SetError(tbPendidikanTerakhir, "");
+                    epWrong.SetError(tbPendidikanTerakhir, "");
+                }
+                else
+                {
+                    epCorrect.SetError(tbPendidikanTerakhir, "");
+                    epWarning.SetError(tbPendidikanTerakhir, "");
+                    epWrong.SetError(tbPendidikanTerakhir, "Inputan Hanya Boleh Huruf Besar!");
+                }
+            }
+        }
+
+        private void tbKotaAsal_Leave(object sender, EventArgs e)
+        {
+            if (tbKotaAsal.Text == "")
+            {
+                epWarning.SetError(tbKotaAsal, "Textbox Kota Asal tidak boleh kosong!");
+                epWrong.SetError(tbKotaAsal, "");
+                epCorrect.SetError(tbKotaAsal, "");
+            }
+            else
+            {
+                if ((tbKotaAsal.Text).All(Char.IsLower))
+                {
+                    epCorrect.SetError(tbKotaAsal, "Betul!");
+                    epWarning.SetError(tbKotaAsal, "");
+                    epWrong.SetError(tbKotaAsal, "");
+                }
+                else
+                {
+                    epCorrect.SetError(tbKotaAsal, "");
+                    epWarning.SetError(tbKotaAsal, "");
+                    epWrong.SetError(tbKotaAsal, "Inputan Hanya Boleh Huruf Kecil!");
+                }
+            }
+        }
+
+        private void tbEmail_Leave(object sender, EventArgs e)
+        {
+            if (tbEmail.Text == "")
+            {
+                epWarning.SetError(tbEmail, "Textbox Email tidak boleh kosong!");
+                epWrong.SetError(tbEmail, "");
+                epCorrect.SetError(tbEmail, "");
+            }
+            else
+            {
+                if (Regex.IsMatch(tbEmail.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+                {
+                    epWarning.SetError(tbEmail, "");
+                    epWrong.SetError(tbEmail, "");
+                    epCorrect.SetError(tbEmail, "Betul!");
+                }
+                else
+                {
+                    epWrong.SetError(tbEmail, "Format email salah!\nContoh : a@b.c");
+                    epWarning.SetError(tbEmail, "");
+                    epCorrect.SetError(tbEmail, "");
+                }
+            }
+        }
+
+        private void tbBeratSekarang_Leave(object sender, EventArgs e)
+        {
+            if (tbBeratSekarang.Text == "")
+            {
+                epWarning.SetError(tbBeratSekarang, "Textbox Berat Sekarang tidak boleh kosong!");
+                epWrong.SetError(tbBeratSekarang, "");
+                epCorrect.SetError(tbBeratSekarang, "");
+            }
+            else
+            {
+                if ((tbBeratSekarang.Text).All(Char.IsNumber))
+                {
+                    epCorrect.SetError(tbBeratSekarang, "Betul!");
+                    epWarning.SetError(tbBeratSekarang, "");
+                    epWrong.SetError(tbBeratSekarang, "");
+                }
+                else
+                {
+                    epCorrect.SetError(tbBeratSekarang, "");
+                    epWarning.SetError(tbBeratSekarang, "");
+                    epWrong.SetError(tbBeratSekarang, "Inputan Hanya Boleh Angka!");
+                }
+            }
+        }
+
+        private void tbBeratTahunKemaren_Leave(object sender, EventArgs e)
+        {
+            if (tbBeratTahunKemaren.Text == "")
+            {
+                epWarning.SetError(tbBeratTahunKemaren, "Textbox Berat Tahun Kemaren tidak boleh kosong!");
+                epWrong.SetError(tbBeratTahunKemaren, "");
+                epCorrect.SetError(tbBeratTahunKemaren, "");
+            }
+            else
+            {
+                if ((tbBeratTahunKemaren.Text).All(Char.IsNumber))
+                {
+                    epCorrect.SetError(tbBeratTahunKemaren, "Betul!");
+                    epWarning.SetError(tbBeratTahunKemaren, "");
+                    epWrong.SetError(tbBeratTahunKemaren, "");
+                }
+                else
+                {
+                    epCorrect.SetError(tbBeratTahunKemaren, "");
+                    epWarning.SetError(tbBeratTahunKemaren, "");
+                    epWrong.SetError(tbBeratTahunKemaren, "Inputan Hanya Boleh Angka!");
+                }
+            }
+        }
+
+        private void btTampilkan_Click(object sender, EventArgs e)
+        {
+            int Angka1 = int.Parse(tbBeratSekarang.Text);
+            int Angka2 = int.Parse(tbBeratTahunKemaren.Text);
+
+            //Nama
+            if (tbNama.Text == "")
+            {
+                MessageBox.Show("Harus memasukkan nama anda", "Warning!",
+                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (!(tbNama.Text).All(Char.IsLetter))
+            {
+                MessageBox.Show("Masukkan nama anda dengan benar", "Warning!",
+                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            //Username Valorant
+            else if (tbUsernameValorant.Text == "")
+            {
+                MessageBox.Show("Harus memasukkan username valorant anda", "Warning!",
+                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (!(tbUsernameValorant.Text.Length <= 25))
+            {
+                MessageBox.Show("Masukkan username valorant anda dengan benar", "Warning!",
+                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            //Pendidikan Terakhir
+            else if (tbPendidikanTerakhir.Text == "")
+            {
+                MessageBox.Show("Harus memasukkan pendidikan terakhir anda", "Warning!",
+                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (!(tbPendidikanTerakhir.Text).All(Char.IsUpper))
+            {
+                MessageBox.Show("Masukkan pendidikan terakhir anda dengan benar", "Warning!",
+                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            //Kota Asal
+            else if (tbKotaAsal.Text == "")
+            {
+                MessageBox.Show("Harus memasukkan kota asal anda", "Warning!",
+                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (!(tbKotaAsal.Text).All(Char.IsLower))
+            {
+                MessageBox.Show("Masukkan kota asal anda dengan benar", "Warning!",
+                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            //Email
+            else if (tbEmail.Text == "")
+            {
+                MessageBox.Show("Harus memasukkan email anda", "Warning!",
+                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else if (!Regex.IsMatch(tbEmail.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                MessageBox.Show("Masukkan kota asal anda dengan benar", "Warning!",
+                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            //Berat Sekarang
+            else
+            {
+                if (Angka1 > Angka2) 
+                {
+                    int x = Angka1 - Angka2;
+                    MessageBox.Show
+                    ("Nama\t\t\t: " + tbNama.Text +
+                    "\nUsername Valorant\t: " + tbUsernameValorant.Text +
+                    "\nPendidikan Terakhir\t: " + tbPendidikanTerakhir.Text +
+                    "\nKota Asal\t\t\t: " + tbKotaAsal.Text +
+                    "\nEmail\t\t\t: " + tbEmail.Text +
+                    "\n\nBerat badan anda tahun ini bertambah sebesar " + x + " kilogram",
+                    "Informasi Pendaftaran",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else if (Angka1 < Angka2)
+                {
+                    int y = Angka2 - Angka1;
+
+                    MessageBox.Show
+                    ("Nama\t\t\t: " + tbNama.Text +
+                    "\nUsername Valorant\t: " + tbUsernameValorant.Text +
+                    "\nPendidikan Terakhir\t: " + tbPendidikanTerakhir.Text +
+                    "\nKota Asal\t\t\t: " + tbKotaAsal.Text +
+                    "\nEmail\t\t\t: " + tbEmail.Text +
+                    "\n\nBerat badan anda tahun ini berkurang sebesar " + y + " kilogram",
+                    "Informasi Pendaftaran",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+    }
+}
